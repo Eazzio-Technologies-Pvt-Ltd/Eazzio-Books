e70a30f7df21248bbb9a05d6eee066d337b651a4
Rahul Kumar <147501502+KUmarRahul001@users.noreply.github.com>
2026-06-30 22:38:47 +0530

    Delete frontend-books directory

--- frontend-books/src/components/RuppLogo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const RuppLogo = ({ height = 45 }) => {
-  return (
-    <img src="/logo1.png" alt="Logo" style={{ height: height, width: "auto", objectFit: "contain" }} />
-  );
-};
-
-export default RuppLogo;
--- frontend-books/src/components/skeletons/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Skeleton } from './Skeleton';
-export { default as TableSkeleton } from './TableSkeleton';
-export { default as CardSkeleton } from './CardSkeleton';
-export { default as FormSkeleton } from './FormSkeleton';
-export { default as DetailSkeleton } from './DetailSkeleton';
-export { default as PageSkeleton } from './PageSkeleton';
